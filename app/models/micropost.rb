d7e018948e662c7be4c07239a374ec56703510f3
thlias <thlias2000@gmail.com>
2012-07-26 18:23:38 +0100

    Associated models user and micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ActiveRecord::Base
+  belongs_to :user
+  
   attr_accessible :content, :user_id
+  validates :content, :length => {:maximum => 100}
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :microposts
+  
   attr_accessible :email, :name
+  
+  validates :name, :presence=>:true
+  validates :name, :length=> {:maximum => 100}
 end
